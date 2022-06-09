d37a1e6f79163c6fe6b5affd94897470592b5834
Amri Nuzi <amernuzi@gmail.com>
2022-06-09 15:32:54 +0200

    Featured list

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,4 +1,5 @@
-import Navbar from "../components/navbar/Navbar"
+import Navbar from "../components/navbar/Navbar";
+import Featured from "../components/featured/Featured";
 import "./home.scss"
 
 
@@ -6,44 +7,8 @@ const Home = () => {
   return (
     <div className="home">
       <Navbar/>
-      <img 
-      width="100%"
-        src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" 
-        alt="" 
-      />
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi perferendis accusamus consectetur est, dolorem magni ex enim pariatur. Error perferendis labore odio nesciunt nobis, laborum mollitia reprehenderit. Dolorum, dolores est.
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi perferendis accusamus consectetur est, dolorem magni ex enim pariatur. Error perferendis labore odio nesciunt nobis, laborum mollitia reprehenderit. Dolorum, dolores est.
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi perferendis accusamus consectetur est, dolorem magni ex enim pariatur. Error perferendis labore odio nesciunt nobis, laborum mollitia reprehenderit. Dolorum, dolores est.
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi perferendis accusamus consectetur est, dolorem magni ex enim pariatur. Error perferendis labore odio nesciunt nobis, laborum mollitia reprehenderit. Dolorum, dolores est.
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi perferendis accusamus consectetur est, dolorem magni ex enim pariatur. Error perferendis labore odio nesciunt nobis, laborum mollitia reprehenderit. Dolorum, dolores est.
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi perferendis accusamus consectetur est, dolorem magni ex enim pariatur. Error perferendis labore odio nesciunt nobis, laborum mollitia reprehenderit. Dolorum, dolores est.
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi perferendis accusamus consectetur est, dolorem magni ex enim pariatur. Error perferendis labore odio nesciunt nobis, laborum mollitia reprehenderit. Dolorum, dolores est.
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi perferendis accusamus consectetur est, dolorem magni ex enim pariatur. Error perferendis labore odio nesciunt nobis, laborum mollitia reprehenderit. Dolorum, dolores est.
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi perferendis accusamus consectetur est, dolorem magni ex enim pariatur. Error perferendis labore odio nesciunt nobis, laborum mollitia reprehenderit. Dolorum, dolores est.
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi perferendis accusamus consectetur est, dolorem magni ex enim pariatur. Error perferendis labore odio nesciunt nobis, laborum mollitia reprehenderit. Dolorum, dolores est.
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi perferendis accusamus consectetur est, dolorem magni ex enim pariatur. Error perferendis labore odio nesciunt nobis, laborum mollitia reprehenderit. Dolorum, dolores est.
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi perferendis accusamus consectetur est, dolorem magni ex enim pariatur. Error perferendis labore odio nesciunt nobis, laborum mollitia reprehenderit. Dolorum, dolores est.
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi perferendis accusamus consectetur est, dolorem magni ex enim pariatur. Error perferendis labore odio nesciunt nobis, laborum mollitia reprehenderit. Dolorum, dolores est.
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi perferendis accusamus consectetur est, dolorem magni ex enim pariatur. Error perferendis labore odio nesciunt nobis, laborum mollitia reprehenderit. Dolorum, dolores est.
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi perferendis accusamus consectetur est, dolorem magni ex enim pariatur. Error perferendis labore odio nesciunt nobis, laborum mollitia reprehenderit. Dolorum, dolores est.
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi perferendis accusamus consectetur est, dolorem magni ex enim pariatur. Error perferendis labore odio nesciunt nobis, laborum mollitia reprehenderit. Dolorum, dolores est.
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi perferendis accusamus consectetur est, dolorem magni ex enim pariatur. Error perferendis labore odio nesciunt nobis, laborum mollitia reprehenderit. Dolorum, dolores est.
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi perferendis accusamus consectetur est, dolorem magni ex enim pariatur. Error perferendis labore odio nesciunt nobis, laborum mollitia reprehenderit. Dolorum, dolores est.
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi perferendis accusamus consectetur est, dolorem magni ex enim pariatur. Error perferendis labore odio nesciunt nobis, laborum mollitia reprehenderit. Dolorum, dolores est.
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi perferendis accusamus consectetur est, dolorem magni ex enim pariatur. Error perferendis labore odio nesciunt nobis, laborum mollitia reprehenderit. Dolorum, dolores est.
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi perferendis accusamus consectetur est, dolorem magni ex enim pariatur. Error perferendis labore odio nesciunt nobis, laborum mollitia reprehenderit. Dolorum, dolores est.
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi perferendis accusamus consectetur est, dolorem magni ex enim pariatur. Error perferendis labore odio nesciunt nobis, laborum mollitia reprehenderit. Dolorum, dolores est.
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi perferendis accusamus consectetur est, dolorem magni ex enim pariatur. Error perferendis labore odio nesciunt nobis, laborum mollitia reprehenderit. Dolorum, dolores est.
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi perferendis accusamus consectetur est, dolorem magni ex enim pariatur. Error perferendis labore odio nesciunt nobis, laborum mollitia reprehenderit. Dolorum, dolores est.
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi perferendis accusamus consectetur est, dolorem magni ex enim pariatur. Error perferendis labore odio nesciunt nobis, laborum mollitia reprehenderit. Dolorum, dolores est.
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi perferendis accusamus consectetur est, dolorem magni ex enim pariatur. Error perferendis labore odio nesciunt nobis, laborum mollitia reprehenderit. Dolorum, dolores est.
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi perferendis accusamus consectetur est, dolorem magni ex enim pariatur. Error perferendis labore odio nesciunt nobis, laborum mollitia reprehenderit. Dolorum, dolores est.
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi perferendis accusamus consectetur est, dolorem magni ex enim pariatur. Error perferendis labore odio nesciunt nobis, laborum mollitia reprehenderit. Dolorum, dolores est.
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi perferendis accusamus consectetur est, dolorem magni ex enim pariatur. Error perferendis labore odio nesciunt nobis, laborum mollitia reprehenderit. Dolorum, dolores est.
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi perferendis accusamus consectetur est, dolorem magni ex enim pariatur. Error perferendis labore odio nesciunt nobis, laborum mollitia reprehenderit. Dolorum, dolores est.
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi perferendis accusamus consectetur est, dolorem magni ex enim pariatur. Error perferendis labore odio nesciunt nobis, laborum mollitia reprehenderit. Dolorum, dolores est.
-
-    </div>
+      <Featured type="series"/>
+        </div>
   )
 }
 
