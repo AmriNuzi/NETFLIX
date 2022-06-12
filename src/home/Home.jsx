8f1577535d4cc961dcc740e9e3738ab5218c9338
Amri Nuzi <amernuzi@gmail.com>
2022-06-12 19:18:23 +0200

    Slider-Item

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,13 +1,18 @@
 import Navbar from "../components/navbar/Navbar";
 import Featured from "../components/featured/Featured";
 import "./home.scss"
+import List from "../components/list/List";
 
 
 const Home = () => {
   return (
     <div className="home">
       <Navbar/>
-      <Featured type="series"/>
+      <Featured />
+      <List/>
+      <List/>
+      <List/>
+      <List/>
         </div>
   )
 }
