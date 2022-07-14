09984c31237bb8b85f678f8da07d0a8d4c9e6db1
Amri Nuzi <amernuzi@gmail.com>
2022-07-14 18:46:02 +0200

    Rest Api Movie

--- api/index.js
+++ api/index.js
@@ -3,6 +3,10 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const movieRoute = require("./routes/movies");
+
+
 
 dotenv.config();
 
@@ -14,6 +18,8 @@ mongoose
 app.use(express.json());
 
 app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/moives", movieRoute);
 
 app.listen(8800, ()=>{
     console.log("Backend server is running!");
